bd64e15274d16bced97e554f6714740f453bd4c8
tpluscode <tpluscode@users.noreply.github.com>
2023-04-27 12:17:47 +0200

    local

---
+++ index.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-unused-vars */
+import ecmaScriptLiteralLoader from 'rdf-loader-code/ecmaScriptLiteral.js'
+import ecmaScriptModuleLoader from 'rdf-loader-code/ecmaScriptModule.js'
