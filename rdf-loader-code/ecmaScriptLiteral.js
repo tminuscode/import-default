6167a3193862c970f04bcc69beb9bb8ceb5444ef
tpluscode <tpluscode@users.noreply.github.com>
2023-04-28 08:10:31 +0200

    local copies

---
+++ rdf-loader-code/ecmaScriptLiteral.js
@@ -0,0 +1,16 @@
+let evalTemplateLiteral
+let ns
+
+function loader ({ term }, { context, variables } = {}) {
+  if (!(term.termType !== 'Literal' || !term.datatype.equals(ns.code('EcmaScriptTemplateLiteral')))) {
+    return evalTemplateLiteral(term.value, { context, variables })
+  }
+
+  throw new Error(`Cannot load ES6 literal from term ${term.value}`)
+}
+
+loader.register = registry => {
+  registry.registerLiteralLoader(ns.code('EcmaScriptTemplateLiteral'), loader)
+}
+
+module.exports = loader
---
+++ rdf-loader-code/ecmaScriptModule.js
@@ -0,0 +1,28 @@
+let clownface
+let iriResolve
+let ns
+
+async function loader ({ term, dataset }, { basePath } = {}) {
+  const link = clownface({ term, dataset }).out(ns.code('link'))
+
+  if (link.term && link.term.termType !== 'NamedNode') {
+    throw new Error(`Cannot load ecmaScriptModule from term ${term.value}`)
+  }
+
+  const { filename, method } = iriResolve(link.value, basePath)
+
+  const code = await import(filename)
+
+  if (!method) {
+    return code
+  }
+
+  // split method name by . for deep structures
+  return method.split('.').reduce((code, property) => code[property], code)
+}
+
+loader.register = registry => {
+  registry.registerNodeLoader(ns.code('EcmaScriptModule'), loader)
+}
+
+module.exports = loader
